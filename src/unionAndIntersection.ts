69d07d304e538b1978977e6ffa876b5d220f7b14
Alamin4D <alaminahmed79000@gmail.com>
2026-05-01 12:43:51 +0600

    added union and intersection

---
+++ src/unionAndIntersection.ts
@@ -0,0 +1,63 @@
+type UserRole = "admin" | "user" | "agent";
+
+const checkAccess = (role: UserRole): string => {
+  if (role === "admin") {
+    return "Full Access";
+  } else if (role === "agent") {
+    return "Limited Access";
+  } else {
+    return "User Access";
+  }
+};
+
+console.log(checkAccess("agent"));
+
+// Intersection
+
+type Person = {
+  name: string;
+  age: number;
+};
+
+type Job = {
+  position: string;
+  salary: number;
+};
+
+type Destination ={
+    city: string;
+}
+
+type Employee = Person & Job & Destination;
+
+const emp1: Employee = {
+  name: "Alamin",
+  age: 22,
+  position: "Developer",
+  salary: 50000,
+  city: "Chuadanga",
+};
+
+console.log(emp1);
+
+
+type User = {
+  id: number;
+  email: string;
+};
+
+type Admin = {
+  role: "admin";
+  permissions: string[];
+};
+
+type AdminEmployee = User & Admin;
+
+const admin1: AdminEmployee = {
+  id: 1,
+  email: "admin@gmail.com",
+  role: "admin",
+  permissions: ["manage-users", "view-reports"],
+};
+
+console.log(admin1);
